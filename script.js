ab9045b448e06e610bf2fefa058cb3c602f3ca73
madhumathavi2005-dotcom <madhumathavi2005@gmail.com>
2025-10-23 19:24:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const links = document.querySelectorAll("nav a");
 const pages = {
   home: `<h1>Home Page</h1><p>Welcome to the SPA example!</p>`,
   about: `<h1>About Page</h1><p>This SPA is built using HTML, CSS, and JavaScript.</p>`,
-  contact: `<h1>Contact Page</h1><p>Email: example@mail.com<br>Phone: +91 98765 43210</p>`
+  contact: `<h1>Contact Page</h1><p>Email: madhumathavi2005@gmail.com<br>Phone: +91 9626451963</p>`
 };
 
 // Load default page
@@ -22,3 +22,4 @@ links.forEach(link => {
     link.classList.add("active");
   });
 });
+
